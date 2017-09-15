5a037e9267e13dc0237af75ecd41ca1b7cc88812
andrew.zheng <andy_520123@163.com>
2017-09-15 16:28:50 +0800

    add babel and modify config file.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # vue-learn
-learn how to build spa application by webpack &amp; gulp &amp; es6 &amp;vue/vuex.
+learn how to build spa application with webpack &amp; gulp &amp; es6 &amp;vue/vuex.
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,84 @@
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
 	context: path.resolve(__dirname, 'src/'),
 	entry: {
-		app: './scripts/app.js'
+		app: ['babel-polyfill','./scripts/app.js']
 	},
 	output: {
 		filename: '[name].js',
-		path: path.resolve(__dirname, 'dist/scripts/')
+		path: path.resolve(__dirname, 'dist/scripts/'),
+		publicPath: 'assets/'
 	},
 	module: {
-		loaders: []
+		rules: [{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader', 'postcss-loader']
+		},{
+			test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+			loader: 'url-loader',
+			options: {
+				limit: 10000,
+				name: 'img/[name]_[hash:7].[ext]'
+			}
+		},{
+			test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+			loader: 'url-loader',
+			options: {
+				limit: 10000,
+				name: 'fonts/[name].[hash:7].[ext]'
+			}
+		},{
+			test: /\.css$/,
+			use: ExtractTextPlugin.extract({
+			  fallback: "style-loader",
+			  use: "css-loader"
+			})
+		},{
+			test: /\.vue$/,
+			loader: 'vue-loader',
+			options: {
+			  loaders: {
+				css: ExtractTextPlugin.extract({
+				  use: 'css-loader',
+				  fallback: 'vue-style-loader' // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
+				})
+			  }
+			}
+		}
+	    ],
+		loaders: [ 
+		     test: /\.vue|js$/,
+			 enforce: 'pre',
+			 include: path.resolve(__dirname, 'src'),
+			 exclude: /node_modules/,
+			 use: [{
+				 loader: 'eslint-loader',
+				 options: {
+					 formatter: require('eslint-friendly-formatter')
+				 }
+			 }]
+		]
 	},
 	resolve: {
 		modules:[],
-		alias: {},
-		extensions: []
+		alias: {
+			'vue$': 'vue/dist/vue.js',
+            components: path.resolve(__dirname, 'src/components/'),
+            styles: path.resolve(__dirname, 'src/styles/'),	
+            scripts: path.resolve(__dirname, 'src/scripts/'),
+			views: path.resolve(__dirname, 'src/views/'),
+		},
+		extensions: ['.js', '.json', '.vue', '.css']
 	},
-	plugins: [],
-	devServer: {}
+	plugins: [
+	    new HotModuleReplacementPlugin()
+	],
+	devServer: {
+		contentBase: './src',
+        inline: true,
+        hot: true		
+	}
 };
